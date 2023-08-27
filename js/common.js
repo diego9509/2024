15e02e4d3c5e7d7d285c35df68cac27302146c31
이승원 <seungwon9509@gmail.com>
2023-08-27 12:39:10 +0900

    - add pop accessibility

--- js/common.js
+++ js/common.js
@@ -1,9 +1,21 @@
 $(document).ready(function(){
+    var target = $('.pop-event');
+
     $('.button-event').click(function(){
-        $('.pop-event').addClass('is-shown');
+        target.addClass('is-shown');
+        target.attr('aria-hidden', false);
+        focusControl(0);
     });
 
     $('button.close').click(function(){
-        $('.pop-event').removeClass('is-shown');
+        target.removeClass('is-shown');
+        target.attr('aria-hidden', true);
+        focusControl(-1);
     })
+
+    function focusControl(val) {
+        target.find('button').attr('tabindex', val)
+        target.find('input').attr('tabindex', val)
+        target.find('textarea').attr('tabindex', val)
+    }
 });
